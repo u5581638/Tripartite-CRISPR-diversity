6e1cf11cfe06f2da5572474e87799a276eca09da
u5581638 <44420891+u5581638@users.noreply.github.com>
2024-11-24 15:34:54 +1100

    Add files via upload

---
+++ Chapter_4/1_array_validation/running_scripts/annotation_cas13d_working_prespacers1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#PBS -P do77
+#PBS -q normalsr
+#PBS -l walltime=48:00:00
+#PBS -l mem=52GB
+#PBS -l ncpus=26
+#PBS -l wd
+#PBS -l storage=scratch/xi88+gdata/xi88+gdata/va71
+
+# is there any gpu specific flags
+
+module load blast
+module load python3/3.10.4
+module load samtools
+
+echo $1
+python3 /g/data/va71/crispr_pipeline_annotation/protein_annotation_tool_api_cli_in_progress_no_queries_mapping_draft_debug_full_spacer_control_mds_representatives_pfam_improved_SQL_premapping_v21.py -i /g/data/va71/crispr_pipeline_annotation/annotation_upgraded_main_workflow_run/cas13d/queries/cas13d.fasta -d /g/data/va71/crispr_pipeline_annotation/annotation_upgraded_main_workflow_run/cas13d/genomes/ -t "query" -m "/g/data/va71/labelled_genomes/" -b -r -c -tony -orientation -rnafold_switch -crispr_detect -phage_genomes -PAM_anno -phage_rep -prot_case_generation -cores 26 &> prespacer_cas13d_dump.txt
