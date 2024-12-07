ff692279e29a3122e404e61e6a3e6e8419ecb691
u5581638 <44420891+u5581638@users.noreply.github.com>
2024-12-07 19:39:44 +1100

    deleted superfluous comments

--- Chapter_3/4_cluster_filtering/cRISPR_array_position_appender_working.py
+++ Chapter_3/4_cluster_filtering/cRISPR_array_position_appender_working.py
@@ -43,20 +43,14 @@ def closest_crispr_array (arr_positions, protein_position, first_gene_coord):
 # list of crispr arrs for each genome
 for arr in crispr_arrs:
 	arr_entry = arr.id.split("[")
-	#print(arr_entry)
 	arr_entry = arr_entry[0]
 	if (" " in arr_entry):
 		arr_entry.split(" ")
-	#	print(arr_entry)
 		arr_entry = arr_entry[0] # if no space then entire string
-#		print(arr_entry)
-	if (arr_entry in crispr_arr_dict):
+=	if (arr_entry in crispr_arr_dict):
 		crispr_arr_dict[arr_entry].append(arr)
-		#print("old entry")
 	else:
 		crispr_arr_dict[arr_entry] = [arr]
-		#print("new_entry")
-#need to check this pattern
 index_pattern = re.compile("::[0-9]*:[0-9]*")
 
 ret_seq = []
@@ -64,35 +58,24 @@ ret_seq = []
 i = 0
 for sequ in sequences:
 	returned_seq = re.search(index_pattern, sequ.description)
-#	print(sequ.description)
 
 	genome_id = returned_seq.group(0)
-#	print(genome_id)
-#	print(genome_id)
 	contig_start_coordinate = genome_id
-#	print(contig_start_coordinate)
-	genome_id = sequ.description.split(genome_id)
+=	genome_id = sequ.description.split(genome_id)
 
 	genome_id = genome_id[0]
 	genome_id = genome_id.split("|")
 	genome_id = genome_id[-1]
-	#print(genome_id, sequ.description)
 	contig_start_coordinate = contig_start_coordinate[2:]
 	contig_start_coordinate = contig_start_coordinate.split(":")
-#	print("contig start coordinate:")
-#	print(contig_start_coordinate)
 	contig_start_coordinate = contig_start_coordinate[0] # first coordinate of contig
-	#genome_id = genome_id[1:] # genome id
 	
 	if (genome_id == None):
 		print("error in pattern matching!")
 	genome_id = str(genome_id)
 	if (genome_id in crispr_arr_dict):
 		crispr_arr_sequences = crispr_arr_dict[genome_id] # id for crispr array including position
-	#	print("mapped!")
 		# make distance calculation here! 
-		# should be 
-		# need to find absolute position of protein
 		protein_position = sequ.id.split("|")
 		protein_position2 = protein_position [5]
 		protein_position = protein_position[4]
@@ -104,10 +87,7 @@ for sequ in sequences:
 
 
 			arr_entry = arr_entry.description.split("Pos=")
-		#	print (crispr_arr_sequences)
-		#	print(arr_entry)
 			arr_entry = arr_entry[1]
-		#	print(arr_entry)
 			arr_entry = arr_entry.split("]")
 			arr_entry = arr_entry[0]
 			arr_positions.append(int(arr_entry))
@@ -119,12 +99,10 @@ for sequ in sequences:
 
 		sequ_cpy = sequ
 		sequ_cpy.description += " " + "CRISPR_Position=" + str(nearest_arr_position) + " " + "Distance=" + str(nearest_arr_distance) 
-	#	print (sequ_cpy)
 		ret_seq.append(sequ_cpy)
 		i += 1
 		if (i % 1000 == 0):
 			print(i)
-		#print("Good!")
 	else:	
 		print("error, can't locate array from id!")
 		print(genome_id, sequ.description)
--- Chapter_3/4_cluster_filtering/cluster_size_threshold_filterer.py
+++ Chapter_3/4_cluster_filtering/cluster_size_threshold_filterer.py
@@ -18,7 +18,6 @@ for sequ in sequences:
 	this_sequ = sequ.description.split("cluster_")
 	this_sequ = this_sequ[-1]
 	if (this_sequ != previous_sequ):
-		print(cluster_size)
 		if (cluster_size >= cluster_size_cutoff):
 			print("cluster size is:" + str(len(cluster)))
 			SeqIO.write(cluster, out_file, "fasta")
